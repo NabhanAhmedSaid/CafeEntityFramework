3786f194a580da97d786689355da70daa337d275
safcsp dev <dev@test.com>
2024-02-18 16:33:29 +0400

    Add project files.

---
+++ CafeEntityFramework/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeEntityFramework
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string? CoffeeName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ CafeEntityFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CafeEntityFramework;
+
+using (var context = new AppContextDb())
+{
+    Console.WriteLine("Enter Name of Coffee");
+    var name = Console.ReadLine();
+    Console.WriteLine("Enter the price");
+    var price = decimal.Parse(Console.ReadLine());
+
+    var coffee = new Coffee()
+    {
+        
+        Id = 1,
+        
+        CoffeeName = name,
+        Price = price
+    };
+    context.Coffees.Add(coffee);
+    context.SaveChanges();
+
+    var allcoffees = context.Coffees.ToList();
+
+}
